aec41000367bf7a46e4fd165d2d6debd5e7303f6
Henrique Fickert <henriquefickert@unip.br>
2023-01-27 18:21:48 -0300

    Atualiações do Menu lateral

--- vitrine.site/scripts/home.js
+++ vitrine.site/scripts/home.js
@@ -6,7 +6,7 @@ var totalPagina;
 GetProductData()
 
 async function GetProductData() {
-    request = await GetData(`produtos?NumeroPagina=${index}&ResultadosExibidos=5`);
+    request = await GetData(`produtos?NumeroPagina=${index}&ResultadosExibidos=6`);
     totalPagina = request.dados.dados.totalPaginas;
     let productList = request.dados.pagina;
     CreateCard(productList);
@@ -16,15 +16,15 @@ function CreateCard(result) {
     cardElement = document.getElementById("list-cards");
 
     result.forEach(element => {
-        cardElement.innerHTML += `<li id ="${element.id}"class="list__cards__card">
-        <div class="list__cards__card__image"></div>
-        <div class="list__cards__card__text">
-            <h2 class="list__cards__card__text__titulo">${element.nome}</h2>
-            <p class="list__cards__card__text__descricao">At vero eos et accusamus et iusto odio
+        cardElement.innerHTML += `<li id ="${element.id}"class="card">
+        <div class="card__image"></div>
+        <div class="card__text">
+            <h2 class="card__text-titulo">${element.nome}</h2>
+            <p class="card__text-descricao">At vero eos et accusamus et iusto odio
                 dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos
                 dolores et quas.</p>
-            <p class="list__cards__card__text__quantidade">Qnt: ${element.quantidade}</p>
-            <h3 class="list__cards__card__text__valor">R$ ${element.valor}</h3>
+            <p class="card__text-quantidade">Qnt: ${element.quantidade}</p>
+            <h3 class="card__text-valor">R$ ${element.valor}</h3>
         </div>
         </li>`;
     });
