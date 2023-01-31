072c7d855a3bd396c0c9eccb40349aa86d98d169
Henrique Fickert <henriquefickert@hotmail.com>
2023-01-31 19:01:10 -0300

    Implementação dos codigos auxiliares de Upload de Imagem

---
+++ VitrineAPI/VitrineAPI.Application/Utilities/Paths/DateInformations.cs
@@ -0,0 +1,20 @@
+﻿using VitrineAPI.Domain.Enum;
+
+namespace VitrineAPI.Application.Utilities.Paths
+{
+    public static class DateInformations
+    {
+        public static string GetSplitData(Date data)
+        {
+            DateTime datevalue = DateTime.Now;
+
+            return data switch
+            {
+                Date.Year => datevalue.Year.ToString(),
+                Date.Month => datevalue.Month.ToString(),
+                Date.Day => datevalue.Day.ToString(),
+                _ => null,
+            };
+        }
+    }
+}
